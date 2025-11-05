b51051d5017543bf96c700d5addbfe327bbc0bb0
Tobias <tobiasdcxn@gmail.com>
2025-11-05 20:13:30 -0300

    se eliminó la dependencia spring security

--- gestion/src/main/java/com/backend/gestion/controllers/ClienteController.java
+++ gestion/src/main/java/com/backend/gestion/controllers/ClienteController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import com.backend.gestion.entities.Cliente;
@@ -26,7 +27,7 @@ public class ClienteController {
     }
 
     @PostMapping
-    public ResponseEntity<Cliente> createCliente(Cliente cliente) {
+    public ResponseEntity<Cliente> createCliente(@RequestBody Cliente cliente) {
         return ResponseEntity.ok(service.createCliente(cliente));
     
     }
